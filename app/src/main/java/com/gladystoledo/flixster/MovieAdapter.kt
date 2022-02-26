b87d3076853bec797625f8d49a1ad2d1167d231d
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2022-02-26 01:19:30 -0600

    Add Glide Library to load images to view

--- app/src/main/java/com/gladystoledo/flixster/MovieAdapter.kt
+++ app/src/main/java/com/gladystoledo/flixster/MovieAdapter.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class MovieAdapter(private val context: Context, private val movies: List<Movie>) :
     RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
-
-
+    
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
         return ViewHolder(view)
@@ -28,12 +28,12 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
-        private val tvPoster = itemView.findViewById<ImageView>(R.id.tvPoster)
+        private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
 
         fun bind(movie: Movie){
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
-            //TODO: populate imageView
+            Glide.with(context).load(movie.posterImageURL).into(ivPoster)
         }
     }
 
