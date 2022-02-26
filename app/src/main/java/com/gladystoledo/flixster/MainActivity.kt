c2b1b869f4b7a353b1bc3314683059f6a26deece
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2022-02-25 18:44:54 -0600

    Added Networking+Fetching Data

--- app/src/main/java/com/gladystoledo/flixster/MainActivity.kt
+++ app/src/main/java/com/gladystoledo/flixster/MainActivity.kt
@@ -2,10 +2,35 @@ package com.gladystoledo.flixster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
+private const val TAG = "MainActivity"
+private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val client = AsyncHttpClient()
+        client.get(NOW_PLAYING_URL, object: JsonHttpResponseHandler(){
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure: $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.i(TAG, "onSuccess: JSON data $json")
+                val movieJsonArray = json.jsonObject.getJSONArray("results")
+
+            }
+
+        })
     }
 }
